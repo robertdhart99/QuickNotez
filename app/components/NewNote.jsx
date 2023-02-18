37eca414e0ebe3c72e0d280e2ddb851a56ddc431
Robert Hart <robertdhart99@gmail.com>
2023-02-18 18:11:36 -0500

    added basic validation and button disable when it is pressed.

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,8 +1,17 @@
 import styles from './NewNote.css';
+import { Form, useActionData, useNavigation } from '@remix-run/react';
 
+
+//Form is remix and form is default. in the lines below i sub the default for the more powerful remix Form. keeps the page from being re downloaded each time. and just updates the current one
 function NewNote() {
+    const data = useActionData();
+    const navigation = useNavigation();
+
+    const isSubmitting = navigation.state === 'submitting';
+
     return (
-        <form method="post" id="note-form">
+        <Form method="post" id="note-form">
+            {data?.message && <p>{data.message}</p> }
             <p>
                 <label htmlFor="title">Title</label>
                 <input type="text" id="title" name="title" required />
@@ -12,9 +21,11 @@ function NewNote() {
                 <textarea id="content" name="content" rows="5" required />
             </p>
             <div className="form-actions">
-                <button>Add Note</button>
+                <button disabled={isSubmitting}> {/*checks to see if button was pressed and if it was it disables it so it cant be spammed */}
+                    {isSubmitting ? 'Adding...' : 'Add Note'}
+                </button> 
             </div>
-        </form>
+        </Form>
     );
 }
 
