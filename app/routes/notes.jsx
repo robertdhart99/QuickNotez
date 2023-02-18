fafdf20632578f29077b44ede10a2d2628492196
Robert Hart <robertdhart99@gmail.com>
2023-02-18 17:06:11 -0500

    added back end code to pull in user input for notes and sends to json file to save it. the json file acts as the database. added some methods also to handle page re routing

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,7 @@
-import NewNote, {links as newNoteLinks } from '~/components/NewNote';
+import { redirect } from '@remix-run/node';
 
+import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+import { getStoredNotes, storeNotes } from '~/data/notes';
 
 export default function NotesPage() {
     return (
@@ -7,6 +9,19 @@ export default function NotesPage() {
             <NewNote />
         </main>
     );
+}
+
+//backend code 
+export async function action({request}) { 
+    const formData = await request.formData(); // backend pulls data submitted by form 
+    const noteData = Object.fromEntries(formData); // converts formdata object into plain javascript object (these 2 lines basically pull in user input)
+    //add validation
+    const existingNotes = await getStoredNotes(); //gets ahold of existing noters in the json file
+    noteData.id = new Date().toISOString(); // .id so unique identifier for each note 
+    const updatedNotes = existingNotes.concat(noteData); // creates updated notes object using existingNotes and concatinating note data with new note
+    await storeNotes(updatedNotes); // returns a promise so add await in case we want to redirect - the line below
+    return redirect('/notes'); //redirects to /notes page
+
 }
 
 export function links() {
